37a0a7abd654c95b7c0097ef493f2623492793eb
Norbert Fila <nubet2137@gmail.com>
2026-05-25 22:09:59 +0200

    feat(contact): add modular axios submission pipeline

---
+++ src/features/contact/contactErrors.ts
@@ -0,0 +1,8 @@
+import { AppError } from '../../shared/errors/AppError'
+
+export class ContactFormError extends AppError {
+  constructor(message: string, code = 'CONTACT_FORM_ERROR', details?: unknown) {
+    super(message, code, details)
+    this.name = 'ContactFormError'
+  }
+}
