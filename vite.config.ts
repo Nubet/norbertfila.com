fb66b07a67c1e1e34be7f62a69f6173f6375ade0
Norbert Fila <nubet2137@gmail.com>
2026-05-27 16:03:26 +0200

    ci(security): add phased security headers and CSP injection to Oracle nginx deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const enableAsyncCss = env.VITE_ENABLE_ASYNC_CSS !== 'false'
+  loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [
-      react(),
-      ...(enableAsyncCss
-        ? [
-            {
-              name: 'async-css-delivery',
-              transformIndexHtml(html: string) {
-                return html.replace(
-                  /<link rel="stylesheet" crossorigin href="([^"]+)">/g,
-                  '<link rel="preload" as="style" crossorigin href="$1" onload="this.onload=null;this.rel=\'stylesheet\'">\n    <noscript><link rel="stylesheet" crossorigin href="$1"></noscript>'
-                )
-              },
-            },
-          ]
-        : []),
-    ],
+    plugins: [react()],
     base: '/',
   }
 })
