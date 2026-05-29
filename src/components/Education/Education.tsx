2fbe34dc04f9c216c5ce9c5e5d510fd270ac45c5
Norbert Fila <nubet2137@gmail.com>
2026-05-29 22:19:46 +0200

    feat: make certification cards clickable, open in new tab

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -14,14 +14,16 @@ const education = [
 
 const certifications = [
   {
-    name: 'InfoSukces Laureate',
+    name: 'Laureat Konkursu InfoSukces',
     issuer: 'Politechnika Łódzka',
     year: '2024',
+    url: 'https://media.licdn.com/dms/image/v2/D4D2DAQH0WAgrkgtAaA/profile-treasury-image-shrink_1280_1280/B4DZeDYEu9HkAQ-/0/1750255833350?e=1780693200&v=beta&t=tpxtfeRHRI42ttukVyWLyrFzqMHxymnJ1jfUVeGxy3A',
   },
   {
     name: 'CCNA: Intro to Networks',
     issuer: 'Cisco Networking Academy',
     year: '2025',
+    url: 'https://www.credly.com/badges/3f1f196f-1856-41b8-aed8-bca30de2a927/public_url',
   },
 ]
 
@@ -58,13 +60,19 @@ export default function Education() {
           <h3 className={styles.sidebarTitle}>CERTYFIKATY</h3>
           <div className={styles.certList}>
             {certifications.map((cert) => (
-              <div className={styles.certCard} key={cert.name}>
+              <a
+                href={cert.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.certCard}
+                key={cert.name}
+              >
                 <div className={styles.certHeader}>
                   <h4 className={styles.certName}>{cert.name}</h4>
                   <span className={styles.certYear}>{cert.year}</span>
                 </div>
                 <p className={styles.certIssuer}>{cert.issuer}</p>
-              </div>
+              </a>
             ))}
           </div>
         </div>
